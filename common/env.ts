051c4455b823ea13e6203ec1faf54a90f6bde8fe
Yash Sehgal <yashsehgal.work@gmail.com>
2024-02-28 08:06:45 +0530

    📈 Add posthog config and setup

---
+++ common/env.ts
@@ -0,0 +1,5 @@
+export const POSTHOG_HOST = process.env["NEXT_PUBLIC_POSTHOG_HOST"] as string;
+export const POSTHOG_KEY = process.env["NEXT_PUBLIC_POSTHOG_KEY"] as string;
+export const CURRENT_ENVIRONMENT = process.env[
+  "NEXT_PUBLIC_ENVIRONMENT"
+] as string;
