87b3357183348dc994927e7f1f9b24f0125a8445
Yash Sehgal <yashsehgal.work@gmail.com>
2024-02-28 17:03:04 +0530

    ✨ Add button component variants and states

--- components/sections/website-navigation.tsx
+++ components/sections/website-navigation.tsx
@@ -1,15 +1,21 @@
 import Link from 'next/link';
 import { ResponsiveGridControl } from '../layouts/responsive-grid-control';
 import { NavbarLogo } from '../ui/navbar-logo';
+import { Button } from '../ui/button';
+import { useState } from 'react';
 
 export default function WebsiteNavigation() {
   return (
     <ResponsiveGridControl>
-      <nav className="website-navigation p-4">
+      <nav className="website-navigation p-4 flex flex-row items-center justify-between">
         <div className="logo-wrapper">
           <Link href={'/'}>
             <NavbarLogo responsive />
           </Link>
+        </div>
+        <div className="navbar-options flex flex-row items-center justify-end gap-4">
+          <Button variant="secondary">See Examples</Button>
+          <Button>Start Building</Button>
         </div>
       </nav>
     </ResponsiveGridControl>
---
+++ components/ui/button.tsx
@@ -0,0 +1,117 @@
+'use client';
+import { cn } from '@/helpers/utils';
+import { ArrowRight, ChevronRight, Loader2 } from 'lucide-react';
+import { ReactNode, forwardRef, useState } from 'react';
+
+import { motion } from 'framer-motion';
+
+export type ButtonVariantType = 'primary' | 'secondary' | 'ghost';
+export type ButtonSizeType = 'sm' | 'md' | 'lg';
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  withArrow?: boolean;
+  icon?: ReactNode;
+  iconDirection?: 'left' | 'right';
+  variant?: ButtonVariantType;
+  size?: ButtonSizeType;
+  stretch?: boolean;
+  isLoading?: boolean;
+}
+
+const ButtonVariantStyles: Record<ButtonVariantType, string> = {
+  primary:
+    'bg-gradient-to-b from-blue-500 to-blue-600 text-white shadow-md active:shadow',
+  secondary: 'bg-neutral-800 focus:ring-neutral-700',
+  ghost: 'bg-transparent hover:bg-neutral-800',
+};
+
+const ButtonSizeStyles: Record<ButtonSizeType, string> = {
+  sm: 'text-xs px-4 py-2 rounded-lg',
+  md: 'text-sm px-4 py-2 rounded-lg',
+  lg: 'text-base px-6 py-3 rounded-xl',
+};
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      children,
+      variant = 'primary',
+      size = 'md',
+      icon = null,
+      iconDirection = 'left',
+      withArrow = false,
+      stretch = false,
+      disabled = false,
+      isLoading = false,
+      ...args
+    },
+    ref,
+  ) => {
+    const [hovering, setHovering] = useState<boolean>(false);
+    return (
+      <button
+        ref={ref}
+        className={cn(
+          'tracking-tight font-medium flex flex-row items-center justify-center gap-1 hover:gap-1.5 outline-none transition-all',
+          !disabled &&
+            'hover:brightness-110 active:brightness-90 active:scale-90',
+          ButtonVariantStyles[variant],
+          ButtonSizeStyles[size],
+          className,
+          stretch && 'w-full',
+          isLoading && 'cursor-default opacity-60',
+          disabled && 'cursor-not-allowed opacity-40',
+        )}
+        onMouseEnter={(event) => {
+          // toggling from chevron to arrow
+          if (withArrow) setHovering(true);
+          // Run the function passed in args (if it exists)
+          if (args && args.onMouseEnter) {
+            args.onMouseEnter(event);
+          }
+        }}
+        onMouseLeave={(event) => {
+          // toggling back from arrow to chevron
+          setHovering(false);
+          // Run the function passed in args (if it exists)
+          if (args && args.onMouseLeave) {
+            args.onMouseLeave(event);
+          }
+        }}
+        {...args}>
+        {isLoading ? (
+          <motion.span
+            key={'loader-wrapper'}
+            initial={{
+              opacity: 0,
+              y: 12,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}>
+            <Loader2 className="h-4 w-4 animate-spin" />
+          </motion.span>
+        ) : (
+          <>
+            <span className="button-content-wrapper">
+              {iconDirection === 'left' && icon}
+              {children}
+              {iconDirection === 'right' && icon}
+            </span>
+            {withArrow &&
+              (!hovering ? (
+                <ChevronRight className="h-4 w-4" />
+              ) : (
+                <ArrowRight className="h-4 w-4 animate-pulse" />
+              ))}
+          </>
+        )}
+      </button>
+    );
+  },
+);
+
+Button.displayName = 'Button';
