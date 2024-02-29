e8b41a301efdc374d958abab80ec4b9d48477022
Yash Sehgal <yashsehgal.work@gmail.com>
2024-02-29 14:34:09 +0530

    💄 Update cookies logo

--- components/ui/navbar-logo.tsx
+++ components/ui/navbar-logo.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import { forwardRef, useState } from 'react';
 import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 export interface NavbarLogoProps extends React.HTMLAttributes<HTMLDivElement> {
   responsive?: boolean;
@@ -10,11 +11,20 @@ export interface NavbarLogoProps extends React.HTMLAttributes<HTMLDivElement> {
 export const NavbarLogo = forwardRef<HTMLImageElement, NavbarLogoProps>(
   ({ className, responsive = false, ...args }, ref) => {
     const [hovering, setHovering] = useState<boolean>(false);
+    const pathname = usePathname();
     return (
       <div
         className="w-fit h-fit flex flex-row items-center gap-2 hover:brightness-50"
-        onMouseEnter={() => setHovering(true)}
-        onMouseLeave={() => setHovering(false)}>
+        onMouseEnter={() => {
+          if (pathname !== '/') {
+            setHovering(true);
+          }
+        }}
+        onMouseLeave={() => {
+          if (pathname !== '/') {
+            setHovering(false);
+          }
+        }}>
         <Image
           ref={ref}
           src="/logos/cookies-logo.png"
@@ -23,22 +33,7 @@ export const NavbarLogo = forwardRef<HTMLImageElement, NavbarLogoProps>(
           height={24}
           {...args}
         />
-        {!hovering && (
-          <motion.span
-            key={'logo-text'}
-            className="font-medium"
-            initial={{
-              opacity: 0,
-              y: -12,
-            }}
-            animate={{
-              opacity: 1,
-              y: 0,
-            }}>
-            {'cookies'}
-          </motion.span>
-        )}
-        {hovering && (
+        {hovering && pathname !== '/' && (
           <motion.span
             key={'go-to-home-text'}
             className="font-medium"
