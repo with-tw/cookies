ba11962e49bcc7b5c78686b560166e9a911ea859
Yash Sehgal <yashsehgal.work@gmail.com>
2024-03-04 20:52:00 +0530

    📄 Add docs content for input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -38,6 +38,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         }}
         className={cn(
           'px-3 py-2 rounded-xl border border-transparent bg-white/10 text-sm focus:outline-none focus:bg-white/15',
+          'disabled:cursor-not-allowed disabled:brightness-75',
           className,
         )}
         {...(args as MotionProps)}
