7f9a23b4dededbef8ea4eab3a82ae58c172674a8
Yash Sehgal <yashsehgal.work@gmail.com>
2024-03-10 00:06:10 +0530

    🎨 Prettier code format

--- components/website/cookie-preview.tsx
+++ components/website/cookie-preview.tsx
@@ -1,13 +1,22 @@
-import { cn } from "@/helpers/utils";
-import { forwardRef } from "react";
+import { cn } from '@/helpers/utils';
+import { forwardRef } from 'react';
 
-export interface CookiePreviewProps extends React.HTMLAttributes<HTMLDivElement> {
-}
+export interface CookiePreviewProps
+  extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const CookiePreview = forwardRef<HTMLDivElement, CookiePreviewProps>(
   ({ className, ...args }, ref) => {
-    return <div ref={ref} className={cn("flex flex-row items-center justify-center p-12 rounded-2xl bg-white/10 border border-neutral-100/10 shadow-lg", className)} {...args} />
-  }
-)
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          'flex flex-row items-center justify-center p-12 rounded-2xl bg-white/10 border border-neutral-100/10 shadow-lg',
+          className,
+        )}
+        {...args}
+      />
+    );
+  },
+);
 
-CookiePreview.displayName = "CookiePreview";
+CookiePreview.displayName = 'CookiePreview';
